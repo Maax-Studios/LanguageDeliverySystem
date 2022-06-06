892f602aa57c868d0c8079fc59a8da44ae6d2d86
Maax <97341868+NichtMaax@users.noreply.github.com>
2022-06-06 18:41:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    $servername = "host";
+    $username = "username";
+    $password = "password";
+    $dbname = "database";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if (!$conn) {
+
+            die("Connection failed: " . mysqli_connect_error());
+
+        }
+
+    $error = array(
+        "type" => "error",
+        "msg" => "Language file not found.",
+    );
+
+    if (!$_GET['package']) {
+
+        echo json_encode($error);
+
+    } else {
+
+        $package = $_GET['package'];
+
+
+        $sql = "SELECT * FROM `ms_language` WHERE package = '$package'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+              echo $row["strings"];
+            }
+        } else {
+            echo json_encode($error);
+        }
+
+    }
+?>
